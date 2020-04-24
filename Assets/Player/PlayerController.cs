f9ccb99bfef9aa616a9345fc2b6638630be0ee54
Matthew Dolinka <mattdolinka1@gmail.com>
2020-04-24 17:52:49 -0400

    Tried setting up the player a bit.

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float forwardSpeed = 1f;
+    public float backwardSpeed = 1f;
+    public float strafeSpeed = 1f;
+
+    // the main camera
+    private GameObject cam;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
+        cam = GameObject.Find("MainCamera");
+        rb = gameObject.GetComponent<Rigidbody>();
         
     }
 
@@ -14,5 +26,35 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    
+    private void HandleMovement()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        // adjust motion depending on forwards or backwards
+        float forwardMotion;
+        if (vAxis > 0)
+        {
+            forwardMotion = vAxis * forwardSpeed;
+        }
+        else
+        {
+            forwardMotion = vAxis * backwardSpeed;
+        }
+
+        // how fast we move to the side
+        float strafeMotion = hAxis * strafeSpeed;
+
+        // apply the movement
+        Vector3 movement = new Vector3(strafeMotion, 0f, forwardMotion);
+        rb.AddForce(movement);
     }
 }
