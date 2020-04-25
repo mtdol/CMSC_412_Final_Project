c745ab631c015999faf46957e881efbdaa32b144
Matthew Dolinka <mattdolinka1@gmail.com>
2020-04-25 17:36:39 -0400

    the player can do rudimentary movement now

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float forwardSpeed = 1f;
     public float backwardSpeed = 1f;
     public float strafeSpeed = 1f;
+    public float rotationSpeed = 1f;
+    public float sprintSpeed = 2f;
 
     // the main camera
     private GameObject cam;
@@ -38,23 +40,41 @@ public class PlayerController : MonoBehaviour
     {
         float hAxis = Input.GetAxis("Horizontal");
         float vAxis = Input.GetAxis("Vertical");
+        float rawRotation = Input.GetAxis("Mouse X");
 
         // adjust motion depending on forwards or backwards
         float forwardMotion;
         if (vAxis > 0)
         {
-            forwardMotion = vAxis * forwardSpeed;
+            forwardMotion = vAxis * forwardSpeed * Time.fixedDeltaTime;
         }
         else
         {
-            forwardMotion = vAxis * backwardSpeed;
+            forwardMotion = vAxis * backwardSpeed * Time.fixedDeltaTime;
         }
 
         // how fast we move to the side
-        float strafeMotion = hAxis * strafeSpeed;
+        float strafeMotion = -hAxis * strafeSpeed * Time.fixedDeltaTime;
+
+        float rotation = rawRotation * rotationSpeed * Time.fixedDeltaTime;
+
+        // check if we are running
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            // only run if we are going forward
+            if (vAxis > 0)
+            {
+                forwardMotion *= sprintSpeed;
+            }
+        }
+
+        // apply rotation
+        this.transform.Rotate(0, rotation, 0);
 
         // apply the movement
-        Vector3 movement = new Vector3(strafeMotion, 0f, forwardMotion);
-        rb.AddForce(movement);
+        Vector3 movement = new Vector3(forwardMotion, 0f, strafeMotion);
+        // ensure that we rotate correctly
+        movement = this.transform.TransformDirection(movement);
+        rb.MovePosition(this.transform.position + movement);
     }
 }
