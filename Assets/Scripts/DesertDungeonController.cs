ccfdda1921109bacb0191225be08dfbbf137dac7
Matthew Dolinka <mattdolinka1@gmail.com>
2020-05-02 13:02:00 -0400

    added the new desert dungeon and added a transition to and from in the desert

---
+++ Assets/Scripts/DesertDungeonController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DesertDungeonController : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("collided");
+            SceneManager.LoadScene("Desert Dungeon");
+        }
+        
+    }
+}
---
+++ Assets/Scripts/OverworldReturnController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OverworldReturnController : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("collided");
+            SceneManager.LoadScene("OverWorld");
+        }
+        
+    }
+}
