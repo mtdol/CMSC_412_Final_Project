4cecadeba732d837ca1d17815624cc240d0ccef6
Matthew Dolinka <mattdolinka1@gmail.com>
2020-05-13 17:47:24 -0400

    some changes to the gates in the eoverworld

---
+++ Assets/Scripts/ForestDungeonEntranceController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ForestDungeonEntranceController : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Forest Dungeon");
+        }
+        
+    }
+}
