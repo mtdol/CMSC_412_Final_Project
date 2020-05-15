4d1c7cc10ec071167a1acc6827ee6b546bd509c1
jamccl1 <jmmcclellan98@yahoo.com>
2020-05-14 23:20:12 -0400

    New monster, plus some random things

--- Assets/Monster/MonsterController.cs
+++ Assets/Monster/MonsterController.cs
@@ -5,23 +5,35 @@ using UnityEngine.UI;
 
 public class MonsterController : MonoBehaviour
 {
-    public int maxHealth;
+    public int maxHealth, detectionRadius;
     public Slider healthBarPrefab;
     public Material bodyFadeMaterial, limbFadeMaterial;
     public float walkSpeed;
+    public bool isHeavy;
 
     private int health;
     private Slider healthBar;
     private Animator anim;
     private bool isDead;
-    private GameObject player, topOfHead;
+    private GameObject player, topOfHead, rightHand, leftHand;
 
     // Start is called before the first frame update
     void Start()
     {
-        //topOfHead = GameObject.Find("mixamorig:HeadTop_End");
-        topOfHead = transform.Find("mixamorig:Hips").Find("mixamorig:Spine").Find("mixamorig:Spine1")
-            .Find("mixamorig:Spine2").Find("mixamorig:Neck").Find("mixamorig:Head").Find("mixamorig:HeadTop_End").gameObject;
+        if (isHeavy)
+        {
+            Transform spine = transform.Find("Hips").Find("Spine").Find("Spine1").Find("Spine2");
+            topOfHead = spine.Find("Neck").Find("Head").Find("HeadTop_End").gameObject;
+            rightHand = spine.Find("RightShoulder").Find("RightArm").Find("RightForeArm").Find("RightHand").gameObject;
+            leftHand = spine.Find("LeftShoulder").Find("LeftArm").Find("LeftForeArm").Find("LeftHand").gameObject;
+        }
+        else
+        {
+            Transform spine = transform.Find("mixamorig:Hips").Find("mixamorig:Spine").Find("mixamorig:Spine1").Find("mixamorig:Spine2");
+            topOfHead = spine.Find("mixamorig:Neck").Find("mixamorig:Head").Find("mixamorig:HeadTop_End").gameObject;
+            rightHand = spine.Find("mixamorig:RightShoulder").Find("mixamorig:RightArm").Find("mixamorig:RightForeArm").Find("mixamorig:RightHand").gameObject;
+            leftHand = spine.Find("mixamorig:LeftShoulder").Find("mixamorig:LeftArm").Find("mixamorig:LeftForeArm").Find("mixamorig:LeftHand").gameObject;
+        }
 
         health = maxHealth;
         healthBar = Instantiate(healthBarPrefab, GameObject.Find("Canvas").transform);
@@ -41,42 +53,46 @@ public class MonsterController : MonoBehaviour
         float angle = Vector3.SignedAngle(transform.forward, toPlayer, Vector3.up);
         anim.SetFloat("distanceToPlayer", toPlayer.magnitude);
 
-        // NEED TO COVER CASE THIS HITS WEAPONS
-        RaycastHit hit;
-        Physics.Raycast(topOfHead.transform.position, toPlayer, out hit);
-        String hitName = hit.transform.name;
-
-        // This would be smarter to do with tags, but this makes it easier to merge things on Github
-        bool canSeePlayer = hitName.Equals("Player") || hitName.Equals("SwordCollider") 
-            || hitName.Equals("GreatswordCollider") || hitName.Equals("ShieldCollider");
+        bool canSeePlayer = transform.Find("Ch30").GetComponent<SkinnedMeshRenderer>().isVisible && toPlayer.magnitude <= detectionRadius;
 
         if (healthBar != null)
         {
             healthBar.gameObject.SetActive(canSeePlayer);
-        } 
+        }
 
-        if (!isDead && canSeePlayer && !anim.GetCurrentAnimatorStateInfo(0).IsName("Monster Hit"))
+        if (anim.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
         {
-            if (toPlayer.magnitude > 2.5f)
-            {
-                bool run = (player.GetComponent<PlayerController>().GetWeapon() == Weapon.BOW) && (toPlayer.magnitude > 4);
+            leftHand.GetComponent<BoxCollider>().enabled = !anim.IsInTransition(0);
+            rightHand.GetComponent<BoxCollider>().enabled = !anim.IsInTransition(0);
+        }
+        else
+        {
+            leftHand.GetComponent<BoxCollider>().enabled = false;
+            rightHand.GetComponent<BoxCollider>().enabled = false;
 
-                anim.SetFloat("Movement", Mathf.SmoothStep(anim.GetFloat("Movement"), run ? 1 : 0.5f, 0.2f));
-                transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime * (run ? 3 : 1));
-            } 
+            AnimatorStateInfo animState = anim.GetCurrentAnimatorStateInfo(0);
+
+            if (!isDead && canSeePlayer && !animState.IsName("Monster Hit") && !animState.IsName("Monster Cower"))
+            {
+                if (toPlayer.magnitude > 2.5f)
+                {
+                    anim.SetFloat("Movement", Mathf.SmoothStep(anim.GetFloat("Movement"), 1, 0.2f));
+                    transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime * (isHeavy ? 1.5f : 4));
+                }
+                else
+                {
+                    anim.SetFloat("Movement", Mathf.SmoothStep(anim.GetFloat("Movement"), 0, 0.2f));
+                }
+
+                if (Mathf.Abs(angle) > 15)
+                {
+                    transform.Rotate(new Vector3(0, (isHeavy ? 135 : 270), 0) * Time.deltaTime * Mathf.Sign(angle));
+                }
+            }
             else
             {
                 anim.SetFloat("Movement", Mathf.SmoothStep(anim.GetFloat("Movement"), 0, 0.2f));
             }
-
-            if (Mathf.Abs(angle) > 15)
-            {
-                transform.Rotate(new Vector3(0, 135, 0) * Time.deltaTime * Mathf.Sign(angle));
-            }
-        } 
-        else
-        {
-            anim.SetFloat("Movement", Mathf.SmoothStep(anim.GetFloat("Movement"), 0, 0.2f));
         }
     }
 
@@ -98,7 +114,7 @@ public class MonsterController : MonoBehaviour
 
             IEnumerator death = DeathRoutine(transform.Find("Ch30").GetComponent<SkinnedMeshRenderer>());
             StartCoroutine(death);
-        } 
+        }
         else if (!isDead)
         {
             healthBar.value = health;
@@ -112,20 +128,45 @@ public class MonsterController : MonoBehaviour
     {
         Physics.IgnoreCollision(GetComponent<BoxCollider>(), player.GetComponent<BoxCollider>());
 
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject monster in enemies)
+        {
+            Vector3 distanceToMonster = monster.transform.position - transform.position;
+
+            if (!monster.GetComponent<MonsterController>().isHeavy && distanceToMonster.magnitude <= detectionRadius && monster.gameObject != gameObject)
+            {
+                monster.GetComponent<Animator>().CrossFade("Monster Cower", 0.3f);
+            }
+        }
+
         yield return new WaitForSeconds(5);
 
-        Material[] newMats = { bodyFadeMaterial, limbFadeMaterial };
-        renderer.materials = newMats;
+        if (isHeavy)
+        {
+            renderer.material = bodyFadeMaterial;
+        }
+        else
+        {
+            Material[] newMats = { bodyFadeMaterial, limbFadeMaterial };
+            renderer.materials = newMats;
+        }
 
         float fade = 1f;
         while (fade >= 0.01)
         {
             fade -= 0.02f;
 
-            foreach (Material mat in renderer.materials)
+            if (isHeavy)
             {
-                
-                mat.color = new Color(1f, 1f, 1f, fade);
+                renderer.material.color = new Color(1f, 1f, 1f, fade);
+            }
+            else
+            {
+                foreach (Material mat in renderer.materials)
+                {
+                    mat.color = new Color(1f, 1f, 1f, fade);
+                }
             }
 
             yield return new WaitForFixedUpdate();
