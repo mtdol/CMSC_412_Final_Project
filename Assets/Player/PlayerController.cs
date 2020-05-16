e8e336b6acc300968655f6bd5b58f46c6869cb15
jamccl1 <jmmcclellan98@yahoo.com>
2020-05-16 16:43:39 -0400

    Added quick turn to player

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -46,7 +46,6 @@ public class PlayerController : MonoBehaviour
     public const int DESERT_DUNGEON_SPAWN = 2;
     public const int VILLAGE_ENTRANCE_SPAWN = 3;
 
-
     public float forwardSpeed;
     public float backwardSpeed;
     public float strafeSpeed;
@@ -63,9 +62,10 @@ public class PlayerController : MonoBehaviour
 
     private int health;
 
-    private bool attack, jump, run, shieldUp, aim; // Bools for actions the player can do
+    private bool attack, jump, run, shieldUp, aim, quickTurn; // Bools for actions the player can do
     private bool switchWeapon; // Weapon switching
-    private bool isDead; 
+    private bool isDead;
+    private float quickTurnTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -81,6 +81,7 @@ public class PlayerController : MonoBehaviour
         shieldUp = false;
         switchWeapon = false;
         weapon = Weapon.SWORD;
+        quickTurnTimer = 0;
 
         health = maxHealth;
 
@@ -108,12 +109,13 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-
-        //Debug.Log(HAVE_KEY);
         // Detect if these keys are being held down or not
         run = Input.GetKey(KeyCode.LeftShift);
         shieldUp = Input.GetKey(KeyCode.Mouse1);
         aim = Input.GetKey(KeyCode.Mouse0) && (weapon == Weapon.BOW);
+
+        // Increment the double-tap timer for quick-turning
+        quickTurnTimer = Mathf.Max(quickTurnTimer - Time.deltaTime, 0);
 
         // Detect if these keys have been pressed (but not necessarily held)
         if (Input.GetKeyDown(KeyCode.Space))
@@ -129,6 +131,19 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))
         {
             switchWeapon = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (quickTurnTimer == 0)
+            {
+                quickTurnTimer = 0.5f;
+            } 
+            else
+            {
+                quickTurn = true;
+                quickTurnTimer = 0;
+            }
         }
     }
 
@@ -218,6 +233,15 @@ public class PlayerController : MonoBehaviour
             {
                 anim.SetTrigger("Jump");
                 jump = false;
+            }
+            else if (quickTurn)
+            {
+                anim.SetTrigger("QuickTurn");
+                quickTurn = false;
+            }
+            else if (animState.IsTag("QuickTurn"))
+            {
+                transform.Rotate(new Vector3(0, 420, 0) * Time.deltaTime);
             }
             else if (switchWeapon)
             {
@@ -393,7 +417,6 @@ public class PlayerController : MonoBehaviour
 
 
     }
-
 
     private void Damage(int damageAmount)
     {
