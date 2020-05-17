90eeab75784be3ca88ffa23d616ea3d77bbd1385
Matthew Dolinka <mattdolinka1@gmail.com>
2020-05-17 17:29:08 -0400

    commiting again to see if health pickup appears

---
+++ Assets/Health Pickup/HealthPickupController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickupController : MonoBehaviour
+{
+    public float rotationSpeed;
+    public int healthIncrease;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rotate
+        transform.Rotate (Vector3.up * rotationSpeed * Time.deltaTime, Space.Self);
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // heal the player
+            other.gameObject.GetComponent<PlayerController>().Heal(healthIncrease);
+
+            // deactivate ourself
+            this.gameObject.SetActive(false);
+        }
+    }
+}
